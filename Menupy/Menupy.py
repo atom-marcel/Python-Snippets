030df1512361c61994c042a5cd6e77935dac70ab
Marcel Maluta <marcelmaluta@gmail.com>
2023-11-06 21:53:59 +0100

    Package Menupy hinzugefügt

---
+++ Menupy/Menupy.py
@@ -0,0 +1,63 @@
+from typing import Callable, Dict, List, Any
+
+class Menu(object):
+
+    callstackList: List[Callable] = []
+    callstackDict: Dict[str, Callable] = {}
+    name: str
+
+    calledIndex: int = 0
+    lastInput: str = ""
+
+    def __init__(self, name:str):
+        self.name = name
+
+    def addCallable(self, key: str, call: Callable) -> int:
+        self.callstackList.append(call)
+        self.callstackDict[key] = call
+
+        return [i for i,x in enumerate(self.callstackList) if x == call][0]
+
+    def callIndex(self, index: int) -> Any:
+        return self.callstackList[index]()
+    
+    def callKey(self, key: str) -> Any:
+        return self.callstackDict[key]()
+    
+    def printMenu(self):
+        print(self.name)
+        print("Es gibt folgende Menüpunkte:")
+        for index, call in enumerate(self.callstackList):
+            string = [x for x in self.callstackDict.keys() if self.callstackDict[x] == call][0]
+            print(f"{index}\t{string}")
+
+    def clear(self):
+        print("\033c")
+    
+    def menuInteract(self):
+        self.clear()
+        self.printMenu()
+        self.keyboardInput()
+
+    def invalidInput(self):
+        self.clear()
+        print("Die Eingabe war ungültig, bitte gültige Zahl eingeben.")
+        self.printMenu()
+        self.keyboardInput()
+
+    def keyboardInput(self):
+        self.lastInput = input("Menüpunkt wählen: ")
+        
+        try:
+            self.calledIndex = int(self.lastInput)
+            if(self.calledIndex != None):
+                if(self.calledIndex < len(self.callstackList)):
+                    self.callIndex(self.calledIndex)
+                    return
+                
+            self.invalidInput()
+        except:
+            self.invalidInput()
+
+
+
---
+++ test_Menupy.py
@@ -0,0 +1,14 @@
+from Menupy.Menupy import Menu
+
+def test1():
+    print("Called test1")
+
+def test2():
+    print("Called test2")
+
+if __name__ == "__main__":
+    myMenu = Menu("TestMenu")
+    myMenu.addCallable("test1", test1)
+    myMenu.addCallable("test2", test2)
+
+    myMenu.menuInteract()
