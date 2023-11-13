254e46f72e213c40e00a8832d9f506fdf0097ee4
Marcel Maluta <marcelmaluta@gmail.com>
2023-11-13 16:10:19 +0100

    modified:   Statistics/__pycache__/ww.cpython-311.pyc 	modified:   Statistics/ww.py 	modified:   docker-combine 	modified:   test_ww.py

--- Statistics/ww.py
+++ Statistics/ww.py
@@ -5,6 +5,35 @@ import requests
 import re
 
 class WebWords(object):
+    grammatik: Dict = \
+    {
+        "fuellwoerter": [
+        "aber", "also", "am", "an", "auf", "auch", "aus", "bei", "bin", "bis", 
+        "bist", "da", "dadurch", "daher", "darum", "das", "dass", "denn", 
+        "der", "des", "deshalb", "die", "dies", "diese", "dieser", "dieses", 
+        "doch", "dort", "du", "durch", "ein", "eine", "einem", "einen", 
+        "einer", "eines", "einig", "einige", "einigem", "einigen", "einiger", 
+        "einiges", "er", "es", "etwas", "euch", "euer", "eure", "für", "gegen", 
+        "gewesen", "hab", "habe", "haben", "hat", "hatte", "hatten", "hier", 
+        "hin", "hinter", "ich", "ihr", "ihre", "im", "in", "ist", "ja", "jede", 
+        "jedem", "jeden", "jeder", "jedes", "jene", "jenem", "jenen", "jener", 
+        "jenes", "jetzt", "kann", "kein", "keine", "kennen", "kennst", "können", 
+        "könnt", "machen", "mein", "meine", "mit", "nach", "nachdem", "nein", 
+        "nicht", "nichts", "noch", "nun", "nur", "ob", "oder", "ohne", "sehr", 
+        "sein", "seine", "sich", "sie", "sind", "so", "solche", "solchem", 
+        "solchen", "solcher", "solches", "soll", "sollen", "sondern", "sonst", 
+        "über", "um", "und", "uns", "unser", "unsere", "unter", "viel", "vom", 
+        "von", "vor", "während", "war", "waren", "warst", "was", "weg", "weil", 
+        "weiter", "welche", "welchem", "welchen", "welcher", "welches", "wenn", 
+        "werde", "werden", "wie", "wieder", "will", "wir", "wird", "wirst", 
+        "wo", "wollen", "wollt", "wollte", "würde", "würden", "zu", "zum", 
+        "zur", "zwar", "zwischen", "halb", "je", "wer"
+        ],
+        "ueberschriften_tags": ["h1", "h2", "h3", "h4", "h5", "h6", "title"],
+        "text_tags": ["p", "span", "li", "div"],
+        "satzzeichen": [",", ".", "!", "?", "\"", "&", "§", "/", ";", ":", "-", "*", "~", "+", "(", ")", "[", "]", "{", "}", "–", "|"]
+    }
+
     websites: List[str]
     htmlString: ResultSet[Tag]
     onlyText: str
@@ -25,11 +54,10 @@ class WebWords(object):
         self.htmlString = []
         self.onlyText = ""
 
-        with open("./Statistics/grammatik.json", "r") as f:
-            j = json.loads(f.read())
-            self.filteredWords = j["fuellwoerter"]
-            self.searchTags = j["ueberschriften_tags"] + j["text_tags"]
-            self.punctuation = j["satzzeichen"]
+        j = self.grammatik
+        self.filteredWords = j["fuellwoerter"]
+        self.searchTags = j["ueberschriften_tags"] + j["text_tags"]
+        self.punctuation = j["satzzeichen"]
 
     def crawl(self) -> bool:
         for website in self.websites:
@@ -77,7 +105,7 @@ class WebWords(object):
         for key in self.countDictionary.keys():
             self.percentageDictionary[key] = self.countDictionary[key] / self.count
 
-    def print(self):
+    def printTop(self, skip:int = 0):
         countList = [(key, self.countDictionary[key]) for key in self.countDictionary.keys()]
         countList.sort(key=lambda x: x[1], reverse=True)
 
@@ -93,6 +121,6 @@ class WebWords(object):
         #     print(f"{key} => {(i*100):2.2f}%")
 
         print(f"Top {self.top} der benutzen Wörter")
-        for i in range(self.top):
+        for i in range(skip, self.top):
             if i < len(percentageList) and i < len(countList):
-                print(f"{i+1}. {percentageList[i][0][0].upper() + percentageList[i][0][1:]:25} \t {(percentageList[i][1]*100):2.2f}% \t ({countList[i][1]})")
+                print(f"{i+1-skip}. {percentageList[i][0][0].upper() + percentageList[i][0][1:]:25} \t {(percentageList[i][1]*100):2.2f}% \t ({countList[i][1]})")
--- test_ww.py
+++ test_ww.py
@@ -1,8 +1,8 @@
 from Statistics.ww import WebWords
 
 if __name__ == "__main__":
-    ww = WebWords(["https://www.youtube.com/watch?v=jqOvpVoWzas"])
+    ww = WebWords(["https://www.tagesschau.de/"])
     if ww.crawl():
         ww.analyze()
         ww.top = 30
-        ww.print()
+        ww.printTop()
